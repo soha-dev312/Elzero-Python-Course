7dde66163baa185ff342819b77c431eb211b1821
Sohaila Mohammed <sohila.007419@gmail.com>
2026-08-07 15:02:28 +0300

    Add files via upload

---
+++ Day-01.py
@@ -0,0 +1,24 @@
+#Variables Definition
+
+user_name = "sohaila mohammed"
+user_age = 19
+field = "computer science"
+is_active = True
+#Dynamic Typing Example
+
+current_status = 2026
+current_status = "Learning Python"
+#Print Profile Card Using Escape Sequence \n
+
+print(user_name + "\n" + str(user_age))
+#Print With Horizontal Tab \t
+
+print("Name:\t" + user_name)
+#Print With Escape Double Quotes (\")
+
+print("My Age is \"19\"")
+#Check Data Types Using type() function
+
+print(type(user_name))
+print(type(user_age))
+print(type(is_active))

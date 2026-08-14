abd666dae3feb87de3c9a2ddd9c4fed38d7188c5
Sohaila Mohammed <sohila.007419@gmail.com>
2026-08-14 14:37:37 +0300

    Add files via upload

---
+++ Task_04.py
@@ -0,0 +1,16 @@
+birth_year = int(input("Enter your Birth year?"))
+
+age = 2026-birth_year
+
+print(f"Your age is {age} years ")
+
+print("#" * 50)
+
+agee = int(input("Enter your age?"))
+months = agee * 12
+days = months * 365
+
+print("Your age is:",agee,)
+print("Months:",months,)
+print("Days:",days,)
+print("#" * 50)

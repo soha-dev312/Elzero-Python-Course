1a6932c9e5b7bd597708bd6f0e9dadd12d90886a
Sohaila Mohammed <sohila.007419@gmail.com>
2026-08-14 13:06:15 +0300

    Add files via upload

---
+++ Task_01.py
@@ -0,0 +1,9 @@
+name = input("What's your name?").strip().capitalize()
+age = input("How old are you?").strip()
+uni = input("Enter your university?").strip().title()
+faculty = input("Enter your faculty?").strip().capitalize()
+
+print(f"Your name is :{name}.")
+print(f"Your age is :{age} years old.")
+print(f"University is :{uni}.")
+print(f"Faculty is :{faculty}.")

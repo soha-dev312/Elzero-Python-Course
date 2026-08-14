1d4be51bafa976776b2c797c3bb0710c42d5dd34
Sohaila Mohammed <sohila.007419@gmail.com>
2026-08-14 13:06:36 +0300

    Add files via upload

---
+++ Task_02.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter your first number?"))
+num2 = int(input("Enter your second number?"))
+
+add = num1 + num2
+sub = num1 - num2
+multi = num1 * num2
+division = num1 / num2
+modulus = num1 % num2
+
+print("*" * 50)
+
+print(f"Addition:{add}")
+print(f"Subtraction:{sub}")
+print(f"Muliplication:{multi}")
+print(f"Division:{division}")
+print(f"Modulus:{modulus}")
+
+print("*" * 50)

abbf8149b7f40bc649dc5716d1120674dd2d0c39
Sohaila Mohammed <sohila.007419@gmail.com>
2026-08-15 12:39:48 +0300

    Add files via upload

---
+++ Task_06.py
@@ -0,0 +1,12 @@
+grade = int(input("Enter your exam grade?"))
+
+if grade >= 90:
+    print("Your Grade is Excellent.")
+elif grade >= 75:
+    print("Your Grade is Very Good.")
+elif grade >= 60:
+    print("Your Grade is Good.")
+elif grade >= 50:
+    print("Your Grade is Pass.")        
+else:
+    print("Your Grade is Fall.")    

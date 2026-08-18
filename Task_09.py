bc8a51638c541c6d9a9d37ecf9670b48b92906e1
Sohaila Mohammed <sohila.007419@gmail.com>
2026-08-18 22:49:49 +0300

    Add files via upload

---
+++ Task_09.py
@@ -0,0 +1,13 @@
+user = int(input("Enter a Number:"))
+
+if user > 0:
+    print("The number is positive.")
+elif user < 0:
+    print("The number is negative.")
+else:
+    print("Zero")
+
+if user%2 == 0:
+    print("The number is Even.")
+else:
+    print("The number is Odd.")                
